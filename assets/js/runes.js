77724cc3595eac9ddefd2dbf30720be293c0516c
AntonMitchenko <anton.mitchenko2003@gmail.com>
2023-06-03 14:22:21 +0300

    Added image-map

---
+++ assets/js/runes.js
@@ -0,0 +1,22 @@
+// Получаем все ссылки с якорями
+const links = document.querySelectorAll('a[href^="#"]');
+
+// Обходим каждую ссылку и назначаем обработчик события щелчка
+links.forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault(); // Предотвращаем переход по ссылке
+
+    const target = document.querySelector(this.getAttribute('href')); // Получаем целевой элемент по атрибуту href
+    if (target) {
+      target.scrollIntoView({ behavior: 'smooth' }); // Плавно прокручиваем к целевому элементу
+
+      // Добавляем класс .active, чтобы плавно подсветить элемент
+      target.classList.add('active');
+
+      // Удаляем класс .active после завершения анимации
+      setTimeout(() => {
+        target.classList.remove('active');
+      }, 2000); // Измените значение 1000 на желаемую продолжительность анимации в миллисекундах
+    }
+  });
+});
