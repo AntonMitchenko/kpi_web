be3ad36968d0a6f8b278e5c4f948b40485cc3627
anton.mitchenko <anton.mitchenko2003@gmail.com>
2023-06-04 16:39:36 +0300

    fixed a couple of bugs

--- assets/js/comment.js
+++ assets/js/comment.js
@@ -12,19 +12,3 @@ document.getElementById('submitComment').addEventListener('click', function() {
     commentInput.value = '';
   }
 });
-
-// Old koment box
-//document.getElementById('submitComment').addEventListener('click', function() {
-//  var commentInput = document.getElementById('commentInput');
-//  var commentDisplay = document.getElementById('commentDisplay');
-//
-//  var commentText = commentInput.value;
-//
-//  if (commentText.trim() !== '') {
-//    var commentElement = document.createElement('div');
-//    commentElement.textContent = commentText;
-//    commentDisplay.appendChild(commentElement);
-//
-//    commentInput.value = '';
-//  }
-//});
